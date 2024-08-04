e4695a164481dc0f8cd820dc87dbdd23a70c27e8
QuicklyDG <quicklydeguzman@gmail.com>
2024-08-04 11:20:17 +0800

    commit changes

--- resources/js/app/pages/admin/tickets/sections/ticket-view-section.jsx
+++ resources/js/app/pages/admin/tickets/sections/ticket-view-section.jsx
@@ -10,7 +10,6 @@ import FileTabContentSection from "./file-tab-content-section";
 import ActivitiesTabContentSection from "./activities-tab-content-section";
 import ItPersonnelTabContentSection from "./it-personnel-tab-content-section";
 import { useDispatch, useSelector } from "react-redux";
-import { setPath } from "../redux/tickets-slice";
 
 export default function TicketViewSection() {
     const { path } = useSelector((state) => state.tickets);
@@ -20,90 +19,34 @@ export default function TicketViewSection() {
         <div>
             <div className="flex items-center justify-between flex-1 space-y-4 py-3 px-3 rounded-t-lg bg-slate-500">
                 <div className="mb-4 w-full relative">
-                    <ul
-                        className="flex flex-wrap -mb-px text-sm font-medium text-center"
-                        id="default-tab"
-                        role="tablist"
-                    >
-                        <li
-                            className="me-2 flex-1 relative"
-                            role="presentation"
-                        >
-                            <button
-                                onClick={() => dispatch(setPath("details"))}
-                                className={`flex items-center justify-center hover:bg-white p-4 border-b-2 rounded-t-lg ${
-                                    path === "details" ? "bg-white" : ""
-                                } w-full`}
-                                id="details-tab"
-                                type="button"
-                                role="tab"
-                                aria-controls="details"
-                                aria-selected={path === "details"}
-                            >
-                                <DocumentTextIcon className="h-5 w-5 mr-1" />{" "}
-                                Project Details
+                    <ul className="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab" role="tablist">
+                        <li className="me-3 flex-1 relative" role="presentation">
+                            <button className={`flex items-center justify-center hover:text-white hover:bg-slate-600 p-4 border-b-2 rounded-t-lg ${activeTab === 'profile' ? 'bg-white' : ''} w-full`} id="profile-tab" onClick={() => handleTabClick('profile')} type="button" role="tab" aria-controls="profile" aria-selected={activeTab === 'profile'}>
+                                <DocumentTextIcon className='h-5 w-5 mr-1' /> Project Details
                             </button>
                             {path === "details" && (
                                 <div className="absolute inset-x-0 bottom-0 h-1 bg-white rounded-t-lg"></div>
                             )}
                         </li>
-                        <li
-                            className="me-2 flex-1 relative"
-                            role="presentation"
-                        >
-                            <button
-                                onClick={() => dispatch(setPath("files"))}
-                                className={`flex items-center justify-center hover:bg-white p-4 border-b-2 rounded-t-lg ${
-                                    path === "files" ? "bg-white" : ""
-                                } w-full`}
-                                id="files-tab"
-                                type="button"
-                                role="tab"
-                                aria-controls="files"
-                                aria-selected={path === "files"}
-                            >
-                                <FolderIcon className="h-5 w-5 mr-1" /> Files
+                        <li className="me-3 flex-1 relative" role="presentation">
+                            <button className={`flex items-center justify-center hover:text-white hover:bg-slate-600 p-4 border-b-2 rounded-t-lg ${activeTab === 'dashboard' ? 'bg-white' : ''} w-full`} id="dashboard-tab" onClick={() => handleTabClick('dashboard')} type="button" role="tab" aria-controls="dashboard" aria-selected={activeTab === 'dashboard'}>
+                                <FolderIcon className='h-5 w-5 mr-1' /> Files
                             </button>
                             {path === "files" && (
                                 <div className="absolute inset-x-0 bottom-0 h-1 bg-white rounded-t-lg"></div>
                             )}
                         </li>
-                        <li
-                            className="me-2 flex-1 relative"
-                            role="presentation"
-                        >
-                            <button
-                                onClick={() => dispatch(setPath("activities"))}
-                                className={`flex items-center justify-center hover:bg-white p-4 border-b-2 rounded-t-lg ${
-                                    path === "activities" ? "bg-white" : ""
-                                } w-full`}
-                                id="activities-tab"
-                                type="button"
-                                role="tab"
-                                aria-controls="activities"
-                                aria-selected={path === "activities"}
-                            >
-                                <ClockIcon className="h-5 w-5 mr-1" />{" "}
-                                Activities
+                        <li className="me-3 flex-1 relative" role="presentation">
+                            <button className={`flex items-center justify-center hover:text-white hover:bg-slate-600 p-4 border-b-2 rounded-t-lg ${activeTab === 'settings' ? 'bg-white' : ''} w-full`} id="settings-tab" onClick={() => handleTabClick('settings')} type="button" role="tab" aria-controls="settings" aria-selected={activeTab === 'settings'}>
+                                <ClockIcon className='h-5 w-5 mr-1' /> Activities
                             </button>
                             {path === "activities" && (
                                 <div className="absolute inset-x-0 bottom-0 h-1 bg-white rounded-t-lg"></div>
                             )}
                         </li>
                         <li className="flex-1 relative" role="presentation">
-                            <button
-                                onClick={() => dispatch(setPath("notes"))}
-                                className={`flex items-center justify-center hover:bg-white p-4 border-b-2 rounded-t-lg ${
-                                    path === "notes" ? "bg-white" : ""
-                                } w-full`}
-                                id="notes-tab"
-                                type="button"
-                                role="tab"
-                                aria-controls="notes"
-                                aria-selected={path === "notes"}
-                            >
-                                <PencilSquareIcon className="h-5 w-5 mr-1" />{" "}
-                                I.T Personnel Notes
+                            <button className={`flex items-center justify-center hover:text-white hover:bg-slate-600 p-4 border-b-2 rounded-t-lg ${activeTab === 'contacts' ? 'bg-white' : ''} w-full`} id="contacts-tab" onClick={() => handleTabClick('contacts')} type="button" role="tab" aria-controls="contacts" aria-selected={activeTab === 'contacts'}>
+                                <PencilSquareIcon className='h-5 w-5 mr-1' /> I.T Personnel Notes
                             </button>
                             {path === "notes" && (
                                 <div className="absolute inset-x-0 bottom-0 h-1 bg-white rounded-t-lg"></div>
